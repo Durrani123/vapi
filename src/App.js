5fc2def65b5fa4e7fff912ee151666ef383d8818
unknown <durraniabdurehman@gmail.com>
2024-03-24 16:09:48 +0500

    Initial Code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import buttonConfig from "./buttonConfig";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = "https://cdn.jsdelivr.net/gh/VapiAI/html-script-tag@latest/dist/assets/index.js";
+    script.async = true;
+    document.body.appendChild(script);
+
+    script.onload = () => {
+      window.vapiSDK.run({
+        apiKey: "fdffe3e3-ac8b-4866-b21d-240c6e4b6d58",
+        assistant: "4e152ec3-5496-4aa7-bba8-af06370c0f1c",
+        config: buttonConfig,
+      });
+    };
+ }, []);
+
 }
 
-export default App;
+export default App
